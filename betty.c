4013db09a9a2fc0cbdd8ed3b91a96e30329be0ea
Edwin's <94281852+Mamba-cita@users.noreply.github.com>
2023-05-25 01:06:54 +0300

    Update betty.c

--- betty.c
+++ betty.c
@@ -1,11 +1,1 @@
-#include "shell.h"
-/**
- * sum - adds two numbers
- * @num1: First interger to be added
- * @num2: Second interger to be adedd
- * Return: The total of the sum
- */
-int sum(int num1, int num2)
-{
-	return (num1 + num2);
-}
+
