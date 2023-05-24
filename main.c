5b2d55ece404f4343cb63900bca24fb0f4332683
Edwin's <94281852+Mamba-cita@users.noreply.github.com>
2023-05-25 02:03:47 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,12 +3,7 @@
  * add_sum - Calculates the sum of two integers.
  * @a: The first integer.
  * @b: The second integer.
- *
  * Return: The sum of @a and @b.
- *
- * Description: This function takes two integers @a and @b, and returns
- * their sum. It uses the addition operator to perform the calculation
- * and returns the result.
  */
 int add_sum(int a, int b)
 {
