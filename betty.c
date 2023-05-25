4f395858871ce718e92d04a3ef6d0cc22ebb8737
Mamba-cita <EDWINSJOHN52@gmail.com>
2023-05-25 05:05:34 -0700

    error fixing

---
+++ betty.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/*
+ * sum - calculates the sum of two numbers
+ * @a: The first number
+ * @b: The second number
+ * Return: the result of their sum.
+ */
+int sum(int a, int b)
+{
+	return (a + b);
+}
