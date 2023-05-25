9883ec4cbe45e879352a2152201ce3abee1cc15b
Mamba-cita <EDWINSJOHN52@gmail.com>
2023-05-25 08:51:13 -0700

    getLine.c

---
+++ getLine.c
@@ -0,0 +1,167 @@
+#include "shell.h"
+
+/**
+ * input_buf - Reads input from stdin and stores it in a buffer
+ * @info: Pointer to the info_t struct
+ * @buf: Pointer to the buffer for storing the input
+ * @len: Pointer to the length of the buffer
+ *
+ * Return: The number of characters read
+ */
+ssize_t input_buf(info_t *info, char **buf, size_t *len)
+{
+	ssize_t r = 0;
+	size_t len_p = 0;
+
+	if (!*len) /* If nothing left in the buffer, fill it */
+	{
+		free(*buf);
+		*buf = NULL;
+		signal(SIGINT, sigintHandler);
+#if USE_GETLINE
+		r = getline(buf, &len_p, stdin);
+#else
+		r = _getline(info, buf, &len_p);
+#endif
+		if (r > 0)
+		{
+			if ((*buf)[r - 1] == '\n')
+			{
+				(*buf)[r - 1] = '\0'; /* Remove trailing newline */
+				r--;
+			}
+			info->linecount_flag = 1;
+			remove_comments(*buf);
+			build_history_list(info, *buf, info->histcount++);
+			/* if (_strchr(*buf, ';')) Is this a command chain? */
+			{
+				*len = r;
+				info->cmd_buf = buf;
+			}
+		}
+	}
+	return (r);
+}
+
+/**
+ * get_input - Retrieves input from the buffer and processes command chains
+ * @info: Pointer to the info_t struct
+ *
+ * Return: The number of characters in the current command or buffer length
+ */
+ssize_t get_input(info_t *info)
+{
+	static char *buf; /* The ';' command chain buffer */
+	static size_t i, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(info->arg), *p;
+
+	_putchar(BUF_FLUSH);
+	r = input_buf(info, &buf, &len);
+	if (r == -1) /* EOF */
+		return (-1);
+	if (len) /* We have commands left in the chain buffer */
+	{
+		j = i; /* Init new iterator to current buf position */
+		p = buf + i; /* Get pointer for return */
+
+		check_chain(info, buf, &j, i, len);
+		while (j < len) /* Iterate to semicolon or end */
+		{
+			if (is_chain(info, buf, &j))
+				break;
+			j++;
+		}
+
+		i = j + 1; /* Increment past nulled ';' */
+		if (i >= len) /* Reached end of buffer? */
+		{
+			i = len = 0; /* Reset position and length */
+			info->cmd_buf_type = CMD_NORM;
+		}
+
+		*buf_p = p; /* Pass back pointer to current command position */
+		return (_strlen(p)); /* Return length of current command */
+	}
+
+	*buf_p = buf; /* Not a chain, pass back buffer from _getline() */
+	return (r); /* Return length of buffer from _getline() */
+}
+
+/**
+ * read_buf - Reads input from a file descriptor into a buffer
+ * @info: Pointer to the info_t struct
+ * @buf: Pointer to the buffer for storing the input
+ * @i: Pointer to the current position in the buffer
+ *
+ * Return: The number of characters read
+ */
+ssize_t read_buf(info_t *info, char *buf, size_t *i)
+{
+	ssize_t r = 0;
+
+	if (*i)
+		return (0);
+	r = read(info->readfd, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*i = r;
+	return (r);
+}
+
+/**
+ * _getline - Reads input from a file descriptor and appends it to a buffer
+ * @info: Pointer to the info_t struct
+ * @ptr: Pointer to the buffer for storing the input
+ * @length: Pointer to the length of the buffer
+ *
+ * Return: The number of characters read
+ */
+int _getline(info_t *info, char **ptr, size_t *length)
+{
+	static char buf[READ_BUF_SIZE];
+	static size_t i, len;
+	size_t k;
+	ssize_t r = 0, s = 0;
+	char *p = NULL, *new_p = NULL, *c;
+
+	p = *ptr;
+	if (p && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
+
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
+		return (-1);
+
+	c = _strchr(buf + i, '\n');
+	k = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = _realloc(p, s, s ? s + k : k + 1);
+	if (!new_p) /* MALLOC FAILURE! */
+		return (p ? free(p), -1 : -1);
+
+	if (s)
+		_strncat(new_p, buf + i, k - i);
+	else
+		_strncpy(new_p, buf + i, k - i + 1);
+
+	s += k - i;
+	i = k;
+	p = new_p;
+
+	if (length)
+		*length = s;
+	*ptr = p;
+	return (s);
+}
+
+/**
+ * sigintHandler - Handles the SIGINT signal (Ctrl+C)
+ * @sig_num: Signal number
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
