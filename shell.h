88b515adb9913292cd3d4cc39c303e432c083b11
kaymacharia <kaymachariaa@gmail.com>
2023-05-26 12:35:09 -0700

    up

--- shell.h
+++ shell.h
@@ -226,10 +226,10 @@ list_t *node_starts_with(list_t *, char *, char);
 ssize_t get_node_index(list_t *, list_t *);
 
 /* toem_vars.c */
-int is_chain(info_t *, char *, size_t *);
-void check_chain(info_t *, char *, size_t *, size_t, size_t);
-int replace_alias(info_t *);
-int replace_vars(info_t *);
+int is_chain(kay_t *, char *, size_t *);
+void check_chain(kay_t *, char *, size_t *, size_t, size_t);
+int replace_alias(kay_t *);
+int replace_vars(kay_t *);
 int replace_string(char **, char *);
 
 #endif
